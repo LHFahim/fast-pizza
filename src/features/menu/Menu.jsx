e154a41dbda20783041ca2c4cc198e4b6d8438ba
LHFahim <fahimlutful@gmail.com>
2023-09-22 14:06:37 +0600

    made menu api call

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log("🚀 ~ file: Menu.jsx:6 ~ Menu ~ menu:", menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
 }
+
+export const loader = async () => {
+  return await getMenu();
+};
 
 export default Menu;
